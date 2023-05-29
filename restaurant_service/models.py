f92b903eaa4b5c775dca6ef574e050ef5f6fe481
Alexandr <olexandrzhezheria@gmail.com>
2023-05-29 12:56:36 +0300

    add models to models.py

--- restaurant_service/models.py
+++ restaurant_service/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from django.urls import reverse
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("restaurant_service:cook-detail", kwargs={"pk": self.pk})
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(
+        max_length=1000,
+        help_text="Select a description for this dish..."
+    )
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    def __str__(self):
+        return self.name
