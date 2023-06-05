2bf7c8082ef1552a3c50babf33b83dca7279ebe9
Alexandr <olexandrzhezheria@gmail.com>
2023-06-05 13:24:32 +0300

    create .env, add .env to .gitignore add python-dotenv~=1.0.0 to requirements.txt Add SECRET_KEY variable to env

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -9,17 +9,25 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+# Загрузка переменных окружения из файла .env
+env_path = os.path.join(BASE_DIR, 'restaurant/.env')
+load_dotenv(env_path)
+
+# Получение значения секретного ключа из переменных окружения
+SECRET_KEY = os.getenv('SECRET_KEY')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kw6on&1%b+_bbwfa4#ej(axc4%eo_*tw5fpibe@di*ed^$cs8j"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
